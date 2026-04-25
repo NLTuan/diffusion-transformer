38f41277a65696abc4886edaeb8489328e2ad53b
Le Tuan Huy Nguyen <lthnguyen27@gmail.com>
2026-04-25 17:02:49 -0400

    layer initilializations (stole that pluh)

--- dit/dit.py
+++ dit/dit.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import numpy as np
 from patchify import Patchify, Unpatchify
 from embeddings import TimestepEmbedder, LabelEmbedder
 from dit_block import DiTBlock, FinalLayer
@@ -30,9 +31,50 @@ class DiT(nn.Module):
         ])
 
         self.to_noise = FinalLayer(hidden_dim, patch_size, in_channels)
-    
+        
+        num_patches = (input_size // patch_size) ** 2
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, hidden_dim), requires_grad=False)
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        # Initialize transformer layers:
+        def _basic_init(module):
+            if isinstance(module, nn.Linear):
+                torch.nn.init.xavier_uniform_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+        self.apply(_basic_init)
+
+        # Initialize (and freeze) pos_embed by sin-cos embedding:
+        grid_size = int(self.pos_embed.shape[1] ** 0.5)
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], grid_size)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        # Initialize patch_embed like nn.Linear (instead of nn.Conv2d):
+        w = self.patchify.proj.weight.data
+        nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+        nn.init.constant_(self.patchify.proj.bias, 0)
+
+        # Initialize label embedding table:
+        nn.init.normal_(self.label_embedder.embs.weight, std=0.02)
+
+        # Initialize timestep embedding MLP:
+        nn.init.normal_(self.time_embedder.proj[0].weight, std=0.02)
+        nn.init.normal_(self.time_embedder.proj[2].weight, std=0.02)
+
+        # Zero-out adaLN modulation layers in DiT blocks:
+        for block in self.blocks:
+            nn.init.constant_(block.conditionning_mlp[-1].weight, 0)
+            nn.init.constant_(block.conditionning_mlp[-1].bias, 0)
+
+        # Zero-out output layers:
+        nn.init.constant_(self.to_noise.conditionning_mlp[-1].weight, 0)
+        nn.init.constant_(self.to_noise.conditionning_mlp[-1].bias, 0)
+        nn.init.constant_(self.to_noise.mlp.weight, 0)
+        nn.init.constant_(self.to_noise.mlp.bias, 0)
+
     def forward(self, x, t, y=None):
-        x = self.patchify(x)
+        x = self.patchify(x) + self.pos_embed
         time_embs = self.time_embedder(t)
         label_embs = self.label_embedder(y, self.training)
         cond = time_embs + label_embs
