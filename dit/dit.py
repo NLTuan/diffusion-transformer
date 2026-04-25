a7c68b8581e812eaa68246e1842c1b52789af4eb
Le Tuan Huy Nguyen <lthnguyen27@gmail.com>
2026-04-25 11:32:46 -0400

    unpatchify module

--- dit/dit.py
+++ dit/dit.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
-
+from patchify import Patchify
+from embeddings import TimestepEmbedder, LabelEmbedder
+from dit_block import DiTBlock
 
 class DiT(nn.Module):
     def __init__(
@@ -11,8 +13,24 @@ class DiT(nn.Module):
         time_emb_dim=256,
         cfg_dropout=0.0
         ):
-    
-        pass
+
+        self.patchify = Patchify(hidden_dim, patch_size, in_channels=3)
+        self.unpatchify = Unpatchify(hidden_dim, patch_size)
+
+        self.time_embedder = TimestepEmbedder(time_emb_dim)
+        self.label_embedder = LabelEmbedder(time_emb_dim)
+
+        self.blocks = nn.ModuleList([
+            DiTBlock(hidden_dim, time_emb_dim, cfg_dropout)
+            for _ in range(n_blocks)
+        ])
+
+        self.to_noise = nn.Linear(hidden_dim, hidden_dim)
+
+        
+
+
+        
     
     
     def forward(self, x):
--- dit/patchify.py
+++ dit/patchify.py
@@ -8,10 +8,12 @@ class Patchify(nn.Module):
         Turns an image into a sequence of embedded tokens
     """
     
-    def __init__(self, 
-                 dim=1152, 
-                 patch_size=2,
-                 in_channels=4):
+    def __init__(
+        self, 
+        dim=1152, 
+        patch_size=2,
+        in_channels=4
+    ):
         
         super().__init__()
         self.proj = nn.Conv2d(in_channels, dim, patch_size, stride=patch_size)
@@ -20,9 +22,42 @@ class Patchify(nn.Module):
         x = self.proj(x) # (B, C, W, H) -> (B, hidden_dim, w // patch_size, h // patch_size)
         return rearrange(x, 'b d h w -> b (h w) d') # (B, "seq_len", hidden_dim)
     
+
+class Unpatchify(nn.Module):
+    """
+        Turns a sequence of tokens-like into an image
+    """
+    def __init__(
+        self,
+        patch_size
+    ):
+        super().__init__()
+        self.patch_size = patch_size
+
+
+    def forward(self, x):
+        h = w = int(x.shape[1] ** 0.5)
+
+        x = rearrange(
+            x, 
+            'b (h w) (p1 p2 c) -> b c (h p1) (w p2)', 
+            h=h, 
+            w=w,
+            p1=self.patch_size,
+            p2=self.patch_size
+            )
+        return x
     
 if __name__ == "__main__":
     noised_latent = torch.randn(3, 4, 32, 32)
     
     patchify = Patchify()
-    print(patchify(noised_latent).shape)
+    print('Original shape:', noised_latent.shape)
+    print('Patched shape:', patchify(noised_latent).shape)
+
+    sample_final_mlp = nn.Linear(1152, 2 * 2 * 4)
+    print('Final mlp output shape:', sample_final_mlp(patchify(noised_latent)).shape)
+
+    unpatchify = Unpatchify(2)
+    print('Unpatched shape:', unpatchify(sample_final_mlp(patchify(noised_latent))).shape)
+
